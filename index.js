d04d576584a622d47adb9849ac13e43ca13a3181
msaaddev <mrsaadirfan@gmail.com>
2020-02-17 15:40:08 +0500

    📦 NEW: get

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const app = express();
+
+const courses = [
+	{ id: 1, name: 'course1' },
+	{ id: 2, name: 'course2' },
+	{ id: 3, name: 'course3' }
+];
+
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+});
+
+app.get('/api/courses', (req, res) => {
+	res.send(courses);
+});
+
+app.get('/api/courses/:id', (req, res) => {
+	const course = courses.find(c => c.id === parseInt(req.params.id));
+	!course ? res.status(404).send('The course with the given ID was not found.') : res.send(course);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+	console.log(`Application  is running on port ${PORT}`);
+});
