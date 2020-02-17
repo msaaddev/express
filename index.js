b6498062dbc4a5d9793c713b59bceed2c031fd31
msaaddev <mrsaadirfan@gmail.com>
2020-02-17 17:58:27 +0500

    📦 NEW: Joi Validation

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
 
@@ -23,10 +24,20 @@ app.get('/api/courses/:id', (req, res) => {
 });
 
 app.post('/api/courses', (req, res) => {
-	if (!req.body.name || req.body.name.length < 3) {
-		res.status(400).send('Name is required and should be minimum three characters.');
+	const schema = {
+		name: Joi.string()
+			.min(3)
+			.required()
+	};
+
+	const result = Joi.validate(req.body, schema);
+	console.log(result);
+
+	if (result.error) {
+		res.status(400).send(result.error.details[0].message);
 		return;
 	}
+
 	const course = {
 		id: courses.length + 1,
 		name: req.body.name
