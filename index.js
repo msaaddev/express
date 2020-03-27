1a07c566f6933b74cc61c5d7b0ef5dc4b1eeff45
msaaddev <mrsaadirfan@gmail.com>
2020-03-27 19:47:28 +0500

    📦 NEW: built-in middleware

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const auth = require("./auth");
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.use(logger);
 
